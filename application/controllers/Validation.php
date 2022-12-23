6e537d4243cd91c652ae96c65cdb3c9071f78b69
Dwiki Septiawan 20312059 <dwiki_septiawan@teknokrat.ac.id>
2022-12-23 18:35:58 +0700

    fix validation user

---
+++ application/controllers/Validation.php
@@ -0,0 +1,53 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+// panggil file "Server.php"
+require APPPATH . "libraries/Server.php";
+// require APPPATH."libraries/Server.php";
+
+class Validation extends Server
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        // Your own constructor code
+
+        // panggil model "User"
+        $this->load->model("MUser", "user", TRUE);
+    }
+
+    // buat service "POST"
+    function index_get()
+    {
+        echo "Anda kena Prank";
+    }
+
+    // buat service "POST"
+    function index_post()
+    {
+        // ambil parameter username dan password
+        $username = $this->get("username");
+        $password = $this->get("password");
+        $check = false;
+        $id = "EMPTY";
+        $type = "EMPTY";
+
+        // panggil method "get_data"
+        $hasil = $this->user->get_data();
+
+        foreach ($hasil as $user) {
+            if (strcmp($user->username, $username) == 0 && strcmp($user->password, $password) == 0) {
+                $check = true;
+                $id = $user->id;
+                $type = $user->type;
+            }
+        }
+
+        $this->response([
+            'check' => $check,
+            'id' => base64_encode($id),
+            'type' => $type
+        ], 200);
+    }
+}
