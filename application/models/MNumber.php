cf7891a62e4d5977e62c876d6078abd660cefa4e
M.Bramantio Hendhika R 20312104 <m_bramantio_hendhika_rahmanda@teknokrat.ac.id>
2022-12-23 21:57:03 +0700

    fix model number

--- application/models/MNumber.php
+++ application/models/MNumber.php
@@ -11,46 +11,20 @@ class MNumber extends CI_Model
         $this->db->from("tbl_number");
         return $this->db->get()->result();
     }
-    // buat fungsi untuk delete data
-    function delete_data($id)
+
+    // buat fungsi untuk update data
+    function update_data($number)
     {
         // cek apakah npm ada atau tidak
-        $this->db->select("*");
-        $this->db->from("tbl_gambar");
-        $this->db->where("id = '$id'");
-        $this->db->delete("tbl_gambar");
+        // $this->db->select("npm");
+        // $this->db->from("tb_mahasiswa");
+        // $this->db->where("TO_BASE64(npm) = '$token'");
 
-        return $this->db->get()->result();
-    }
+        $data = array(
+            "id" => "1",
+            "number" => $number
+        );
 
-    // buat fungsi untuk save data
-    function save_data($npm, $nama, $telepon, $jurusan, $token)
-    {
-        // cek apakah npm ada atau tidak
-        $this->db->select("npm");
-        $this->db->from("tb_mahasiswa");
-        $this->db->where("TO_BASE64(npm) = '$token'");
-        //  $this->db->where("npm = '$npm'");
-        // eksekusi query
-        $query = $this->db->get()->result();
-        // jika npm tidak ditemukan
-        if (count($query) == 0) {
-            // isi nilai untuk masing" filed
-            $data = array(
-                "npm" => $npm,
-                "nama" => $nama,
-                "telepon" => $telepon,
-                "jurusan" => $jurusan,
-            );
-
-            // simmoan data
-            $this->db->insert("tb_mahasiswa", $data);
-            $hasil = 0;
-        }
-        //  jika npm ditemukan 
-        else {
-            $hasil = 1;
-        }
-        return $hasil;
+        return $this->db->update("tbl_number", $data);
     }
 }
