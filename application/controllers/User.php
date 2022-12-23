574b1a9df399aeb1a097c92ba74a68ddaa3b9f42
Dwiki Septiawan 20312059 <dwiki_septiawan@teknokrat.ac.id>
2022-12-23 13:50:33 +0700

    update controller user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -25,92 +25,104 @@ class User extends Server
         $password = $this->post("password");
         $check = false;
         $id = "EMPTY";
+        $type = "EMPTY";
 
         // panggil method "get_data"
         $hasil = $this->user->get_data();
 
         foreach ($hasil as $user) {
-            if (strcmp($user['username'], $username) == 0 && strcmp($user['password'], $password) == 0) {
+            if (strcmp($user->username, $username) == 0 && strcmp($user->password, $password) == 0) {
                 $check = true;
-                $id = $user['id'];
+                $id = $user->id;
+                $type = $user->type;
             }
         }
 
         // hasil respon
         $hasil = [
             'check' => $check,
-            'id' => $id
+            'id' => base64_encode($id),
+            'type' => $type
         ];
 
-        $this->response(array("login" => $hasil), 200);
+        // $this->response(array("jos" => "jadi"), 200);
     }
 
     // buat service "POST"
-    function service_post()
+    function input_user()
     {
-        // // panggil model "Mmahasiswa"
-        // $this->load->model("Mmahasiswa","mdl",TRUE);
-        // ambil parameter data yang akan di isi
-        $data = array(
-            "npm" => $this->post("npm"), //array $data[0]
-            "nama" => $this->post("nama"), //array $data[1]
-            "telepon" => $this->post("telepon"), //array $data[2]
-            "jurusan" => $this->post("jurusan"), //array $data[3]
-            "token" => base64_encode($this->post("npm")),
-        );
-        // panggil method "save data"
-        $hasil = $this->mdl->save_data($data["npm"], $data["nama"], $data["telepon"], $data["jurusan"], $data["token"]);
-        // jika hasil = 0
-        if ($hasil == 0) {
-            $this->response(array("status" => "Data Mahasiswa Berhasil Disimpan"), 200);
+        $token = $this->post("token");
+        $nama = $this->post("nama");
+        $username = $this->post("username");
+        $password = $this->post("password");
+        $type = 2;
+        $check = false;
+
+        $hasil = $this->user->get_data();
+
+        foreach ($hasil as $user) {
+            if (strcmp(base64_encode($user->id), $token) == 0) {
+                $check = true;
+            }
         }
-        // jika hasil !=0
+
+        if ($check) $hasil = $this->user->save_data($nama, $username, $password, $type);
+        else $hasil = 0;
+
+        // jika hasil == 1
+        if ($hasil == 1) {
+            $this->response(array("status" => "Data User Berhasil Disimpan"), 200);
+        }
+        // jika hasil == 0
         else {
-            $this->response(array("status" => "Data Mahasiswa Gagal Disimpan"), 200);
+            $this->response(array("status" => "Data User Gagal Disimpan"), 200);
         }
     }
 
-    // buat service "PUT"
-    function service_put()
+    // buat service "POST"
+    function input_user_admin()
     {
-        // // panggil model "Mmahasiswa"
-        // $this->load->model("Mmahasiswa","mdl",TRUE);
-        // ambil parameter data yang akan di isi
-        $data = array(
-            "npm" => $this->put("npm"), //array $data[0]
-            "nama" => $this->put("nama"), //array $data[1]
-            "telepon" => $this->put("telepon"), //array $data[2]
-            "jurusan" => $this->put("jurusan"), //array $data[3]
-            "token" => base64_encode($this->put("token")),
-        );
-        // panggil method "save data"
-        $hasil = $this->mdl->update_data($data["npm"], $data["nama"], $data["telepon"], $data["jurusan"], $data["token"]);
+        $token = $this->post("token");
+        $nama = $this->post("nama");
+        $username = $this->post("username");
+        $password = $this->post("password");
+        $type = 1;
+        $check = false;
 
-        // jika hasil = 0
-        if ($hasil == 0) {
-            $this->response(array("status" => "Data Mahasiswa Berhasil Di Update"), 200);
+        $hasil = $this->user->get_data();
+
+        foreach ($hasil as $user) {
+            if (strcmp(base64_encode($user->id), $token) == 0) {
+                $check = true;
+            }
         }
-        // jika hasil !=0
+
+        if ($check) $hasil = $this->user->save_data($nama, $username, $password, $type);
+        else $hasil = 0;
+
+        // jika hasil == 1
+        if ($hasil == 1) {
+            $this->response(array("status" => "Data User Berhasil Disimpan"), 200);
+        }
+        // jika hasil == 0
         else {
-            $this->response(array("status" => "Data Mahasiswa Gagal Di Update"), 200);
+            $this->response(array("status" => "Data User Gagal Disimpan"), 200);
         }
     }
 
     // buat service "DELETE"
     function service_delete()
     {
-        // // panggil model "Mmahasiswa"
-        // $this->load->model("Mmahasiswa","mdl",TRUE);
-        // ambil parameter token "npm"
-        $token = $this->delete("npm");
-        //    panggil fungsi "delete_data"
-        $hasil = $this->mdl->delete_data(base64_encode($token));
+        $id = $this->post("id");
+
+        $hasil = $this->user->delete_data($id);
+
         if ($hasil == 1) {
-            $this->response(array("status" => "Data Mahasiswa Berhasil Dihapus"), 200);
+            $this->response(array("status" => "Data User Berhasil Dihapus"), 200);
         }
         // jika proses delete gagal
         else {
-            $this->response(array("status" => "Data Mahasiswa Gagal Dihapus"), 200);
+            $this->response(array("status" => "Data User Gagal Dihapus"), 200);
         }
     }
 }
